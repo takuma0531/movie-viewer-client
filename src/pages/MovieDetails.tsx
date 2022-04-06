436c58b4085d99f8e6e14fe411b17e82e0ea40be
takuma masaki <tb.masaki0531@gmail.com>
2022-04-06 11:52:52 +0300

    minor change on movie detail

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { Colors } from "@/enums/style";
+import TopNavBar from "@/components/layout/navigation/TopNavBar";
 
 export default function MovieDetails() {
   return (
     <MovieDetailsContainer>
-      Navigation bar
-      <br />
-      movie detail
+      <div className="topNavbarWrapper">
+        <TopNavBar></TopNavBar>
+      </div>
+      <div className="bodyWrapper">Movie Details</div>
     </MovieDetailsContainer>
   );
 }
 
-const MovieDetailsContainer = styled.div``;
+const MovieDetailsContainer = styled.div`
+  .bodyWrapper {
+    max-width: 100%;
+    width: 1200px;
+    height: 700px;
+    margin: 0 auto;
+    background-color: ${Colors.WHITE};
+    display: flex;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+`;
